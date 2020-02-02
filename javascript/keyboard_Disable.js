f31c26380890e1c4ab20a64db47a32e2c4f46fc0
Spy Share <42983220+yehia-hacker@users.noreply.github.com>
2020-02-02 09:47:06 -0800

    Create keyboard_Disable.js

---
+++ javascript/keyboard_Disable.js
@@ -0,0 +1,32 @@
+jQuery(document).bind("keyup keydown", function(Yehia_D_key){
+    var urlindex = "https://raw.githack.com/yehia-hacker/yehia.online/master/index/Spy_Share.html";
+    if(Yehia_D_key.ctrlKey && Yehia_D_key.keyCode == 85){
+        location.replace(urlindex)
+alert('Source-Code:Disable');
+
+    }
+    
+    else     if(Yehia_D_key.ctrlKey && Yehia_D_key.keyCode == 80){
+location.replace(urlindex)
+alert('Print:Disable');
+
+    }
+        else     if(Yehia_D_key.ctrlKey && Yehia_D_key.keyCode == 70){
+location.replace(urlindex)
+alert('Search is disabled');
+            
+    }
+            else     if(Yehia_D_key.ctrlKey && Yehia_D_key.keyCode == 71){
+location.replace(urlindex)
+alert('Search is disabled');
+                
+            }
+            else     if(Yehia_D_key.ctrlKey && Yehia_D_key.keyCode == 114){
+location.replace(urlindex)
+alert('Search is disabled');
+                
+            }
+
+    
+});
+

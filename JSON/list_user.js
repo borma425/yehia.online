db6ee6d3dcaf99a7166bc2fb4570a18c17780d16
Spy Share <42983220+yehia-hacker@users.noreply.github.com>
2020-03-31 17:39:54 +0200

    Create list_user.js

---
+++ JSON/list_user.js
@@ -0,0 +1,19 @@
+var Users = {
+  "user1" : {
+  "name" : "Fatima",
+  "age" :  "23",
+  "gender" : "female" 
+},
+
+"user2" : {
+  "name" : "Adam",
+  "age" : "27",
+  "gender" : "male"
+},
+
+"user3" : {
+  "name" : "Mohamed",
+  "age" : "30",
+  "gender" : "male"
+}
+}

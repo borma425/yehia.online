8805ee8c825e9ea75728bf5d41663f972ecfbee5
Spy Share <42983220+yehia-hacker@users.noreply.github.com>
2020-03-31 17:36:24 +0200

    Create users.js

---
+++ JSON/users.js
@@ -0,0 +1,34 @@
+var Users_male = [{
+   "name" : "Mohamed",
+   "age" :  "21",
+   "gender" : "male"
+
+},
+{
+   "name" : "Adam",
+   "age" : "25",
+   "gender" : "male"
+
+},
+{
+   "name" : "Ahmed",
+   "age" : "27",
+   "gender" : "male"
+}];
+var Users_female = [{
+   "name" : "Sofia",
+   "age" :  "28",
+   "gender" : "female"
+
+},
+{
+   "name" : "Fatima",
+   "age" : "23",
+   "gender" : "female"
+
+},
+{
+   "name" : "Sarah	",
+   "age" : "32",
+   "gender" : "female"
+}];

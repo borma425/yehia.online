f45f807b97b0ccfd301a105b16f9215475b40371
Spy Share <42983220+yehia-hacker@users.noreply.github.com>
2020-03-31 17:52:49 +0200

    Create Programmers_information.js

---
+++ JSON/Programmers_information.js
@@ -0,0 +1,6 @@
+{"Users":[
+   {"name":"Nicolas Wirth", "age":"86", "language":"Pascal"},
+   {"name":"Guido van Rossum", "age":"64", "language":"Python"},
+   {"name":"Bjarne Stroustrup", "age":"69", "language":"C++"},
+   {"name":"Alan cooper", "age":"67", "language":"Ruby"}
+]}
